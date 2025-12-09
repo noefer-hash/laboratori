baf6f459f2547fe5088c245b824ebd42058ae3ab
noefer-hash <nramirez2@nigul.cide.es>
2025-12-09 13:41:40 +0000

    tr

--- Laboratori 2/compile.sh
+++ Laboratori 2/compile.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-# Renombrar archivos según la primera clase pública que contengan
+# Renombra archivos según la primera clase pública que contengan
 
 for f in *.java; do
+    # Extrae el nombre de la clase pública
     class_name=$(grep -m 1 '^public class ' "$f" | awk '{print $3}')
+    # Quita el ";" final si lo hubiera
+    class_name=${class_name//;/}
+
     if [ -n "$class_name" ]; then
         mv "$f" "$class_name.java"
     fi
@@ -10,7 +14,15 @@ done
 
 # Compilar todos los .java en bin/
 mkdir -p bin
+echo "Compilando..."
 javac *.java -d bin
+
+if [ $? -eq 0 ]; then
+    echo "Compilación completada."
+else
+    echo "Error durante la compilación."
+fi
+
 
 
 
